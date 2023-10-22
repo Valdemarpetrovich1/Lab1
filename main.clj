d32ae32913d15bdebf6431c57e00b86110980d5a
Varfolomey <5033264-Varfolomey@users.noreply.replit.com>
2023-10-22 16:58:25 +0000

    Initial commit

---
+++ main.clj
@@ -0,0 +1,19 @@
+(defn calculate
+  ([array] (calculate array false))
+  ([[start & end :as arr] previous]
+   (when-not (empty? arr)
+     (if (= start :start)
+       (recur end true)
+       (if (or (= start :end) (false? previous))
+         (recur end false)
+         (lazy-seq (cons start (calculate end true))))))))
+
+
+(def data1 [0 0 0 :start 1 2 :end 0 0 0 :start 3 4 :end 0 0 0 :start 5 6 :end])
+(def result1 (vec (calculate data1)))
+(println result1)
+
+
+(def data2 (cons :start (cycle '(0 1 2))))
+(def result2 (take 10 (calculate data2)))
+(println result2)
