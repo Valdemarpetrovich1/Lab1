ea3766f1d53402b1d80fb4c5f9ab1d1219e1e409
Valdemarpetrovich1 <74062546+Valdemarpetrovich1@users.noreply.github.com>
2023-10-25 17:38:16 +0000

    1

--- main.clj
+++ main.clj
@@ -12,3 +12,7 @@
 (def data1 [0 0 0 :start 1 2 :end 0 0 0 :start 3 4 :end 0 0 0 :start 5 6 :end])
 (def result1 (vec (calculate data1)))
 (println result1)
+
+(def data2 (cons :start (cycle '(0 1 2))))
+(def result2 (take 10 (calculate data2)))
+(println result2)
